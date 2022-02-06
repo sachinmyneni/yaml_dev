c6cc4613629c798e5ac4d56ac0a2c113d781e99c
Sachin Myneni <myneni@altair.com>
2022-02-06 02:33:56 -0800

    added second var just to check.

--- yaml_ifelse.py
+++ yaml_ifelse.py
@@ -1,6 +1,15 @@
 import yaml
 import jinja2
 
+def update_from_globals(updates: list) -> None:
+    '''
+    Update a block from global vals
+    Input: List of dicts as input
+           [{'license':'RDFLX=21','H3D'='22'}]
+    For each key, see if the key exists in % % 
+    Replace with the value of that key
+    '''
+    pass
 YAML_FILE = 'ifelse.yml'
 with open(YAML_FILE) as fp:
     dataMap = yaml.safe_load(fp)
@@ -11,15 +20,16 @@ job = yaml.safe_load(template.render(**dataMap))
 print(job)
 print(job['build']['bcygwin_smp']['command'])
 
+global_updates = []
 if 'conditionals' in job:
     for cond in job['conditionals']: # [{'license':{'a'=='a'=21,'a'!='c'=22}},'h3d':{'a'=='a'=14,'a'!='c'=22}]
-        print(cond) #{'license': {'mdl == mdl': 'RDFLX=21', 'mdl != mdl': 'RDFLX=22'}}
+        # print(cond) #{'license': {'mdl == mdl': 'RDFLX=21', 'mdl != mdl': 'RDFLX=22'}}
         for var in cond: 
-            print(var) #license
-            print(cond[var])
+            # print(var) #license
+            # print(cond[var])
             for chks in list(cond[var].items()):
                 if eval(chks[0]):
-                    print(f"{var} = {chks[1]}")
-
-            # for key in list(job['conditionals'][cond][items].keys()):
-            #     print(key) # l i c e n s e 
+                    global_updates.append({var:chks[1]})
+                    # print(f"{var} = {chks[1]}")
+print(global_updates)
+print(job['build']['bcygwin_smp']['command'])
