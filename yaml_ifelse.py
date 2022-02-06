c416d848ee23fc833544889bbcb9a6498c0a4c10
Sachin Myneni <myneni@altair.com>
2022-02-06 00:27:39 -0800

    using jinja to swap out vars.

--- yaml_ifelse.py
+++ yaml_ifelse.py
@@ -1,8 +1,12 @@
 import yaml
 import jinja2
 
-with open('ifelse.yml') as fp:
+YAML_FILE = 'ifelse.yml'
+with open(YAML_FILE) as fp:
     dataMap = yaml.safe_load(fp)
 
-# print(dataMap)
-print(dataMap['branch'])
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath='.'))
+template = env.get_template(YAML_FILE)
+job = yaml.safe_load(template.render(**dataMap))
+print(job)
+print(job['branch'])
